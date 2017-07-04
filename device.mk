e8c052e8432ebf22db8bb0564e850ccbcfb61694
Bharat Pawar <bpawar@codeaurora.org>
2017-07-04 13:01:30 +0530

    msm8996: Enabling full treble on 8996 target
    Change-Id: I072814c2810427688e627d7452d76f35080566ab

--- device.mk
+++ device.mk
@@ -469,6 +469,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
 
+# Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_VENDOR_MOVE_ENABLED := true
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
