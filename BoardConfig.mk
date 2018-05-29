0656b5e3c42c9d6277b3845f88ca963bc854f6ad
padarshr <padarshr@codeaurora.org>
2018-05-29 13:34:14 +0530

    op3: Enable System-As-Root behaviour for non-A/B as well.
    Change-Id: I19b8b8342dc634629427c409764f6cc7d69235d4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -66,6 +66,9 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3154116608
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 57588281344 # 57588297728 - 16384
 BOARD_FLASH_BLOCK_SIZE := 262144
+
+# Enable System As Root even for non-A/B from P onwards
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # Recovery
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := f2fs
