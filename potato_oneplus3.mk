50b0038e6ef94b461c27cd187fb559d034f373ef
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    op3: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- potato_oneplus3.mk
+++ potato_oneplus3.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit Potato configs
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
