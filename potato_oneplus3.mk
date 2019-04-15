da03f5033b900421130f060e6b2bd968d227a8c7
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    op3: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- potato_oneplus3.mk
+++ potato_oneplus3.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit Potato configs
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
