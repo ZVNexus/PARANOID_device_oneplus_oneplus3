099d6ea3831b5fa9c58abd48bad693b0358e2f52
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:14:27 +0430

    op3: inherit product api level makefile
    Change-Id: I970ef7f9a0d352a286395c7cad79dcaf3245f0ab
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- potato_oneplus3.mk
+++ potato_oneplus3.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Product launched with Marshmellow
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
