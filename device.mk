d422d968245c4d7349f67286b55cff978c9d5f82
Jake Weinstein <jake@aospa.co>
2019-11-05 23:03:26 +0100

    op3: Utilize ParanoidDoze.
    Change-Id: Iabd23b440928043d40548106a1fe0f5755511a56

--- device.mk
+++ device.mk
@@ -253,9 +253,9 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
-# Doze mode
+# Doze
 PRODUCT_PACKAGES += \
-    OneplusDoze
+    ParanoidDoze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -269,7 +269,6 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    OneplusPocketMode \
     android.hardware.biometrics.fingerprint@2.1-service
 
 # Gatekeeper HAL
